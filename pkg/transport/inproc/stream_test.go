1b932b8182278cdcab87972e457f4b5863cee068
Louis Thibault <l.thibault@sentimens.com>
2018-11-18 02:25:10 +0000

    Basic testing for inproc streams

---
+++ pkg/transport/inproc/stream_test.go
@@ -0,0 +1,26 @@
+package inproc
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var addrs = ep{local: Addr("/local"), remote: Addr("/remote")}
+
+func TestStreamPair(t *testing.T) {
+	p := newStreamPair(context.Background(), addrs)
+
+	t.Run("LocalStream", func(t *testing.T) {
+		s := p.Local()
+		assert.Equal(t, addrs.Local(), s.Endpoint().Local())
+		assert.Equal(t, addrs.Remote(), s.Endpoint().Remote())
+	})
+
+	t.Run("RemoteStream", func(t *testing.T) {
+		s := p.Remote()
+		assert.Equal(t, addrs.Remote(), s.Endpoint().Local())
+		assert.Equal(t, addrs.Local(), s.Endpoint().Remote())
+	})
+}
