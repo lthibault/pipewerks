31b005ea901a70a5fe8c5b73b9580cb17fb8a7f8
Louis Thibault <l.thibault@sentimens.com>
2018-11-18 06:33:47 +0000

    Retrofit new generic API

--- pkg/transport/tcp/options.go
+++ pkg/transport/tcp/options.go
@@ -3,7 +3,7 @@ package tcp
 import (
 	"net"
 
-	"github.com/hashicorp/yamux"
+	"github.com/lthibault/pipewerks/pkg/transport/generic"
 )
 
 // Option for TCP transport
@@ -11,27 +11,27 @@ type Option func(Transport) (prev Option)
 
 // OptListener sets the ListenConfig
 func OptListener(l *net.ListenConfig) Option {
-	return func(t *Transport) (prev Option) {
-		prev = OptListener(t.Transport.l)
-		t.Transport.l = l
+	return func(t Transport) (prev Option) {
+		prev = OptListener(t.Transport.NetListener.(*net.ListenConfig))
+		t.Transport.NetListener = l
 		return
 	}
 }
 
 // OptDialer sets the dialer
 func OptDialer(d *net.Dialer) Option {
-	return func(t *Transport) (prev Option) {
-		prev = OptDialer(t.Transport.d)
-		t.Transport.d = d
+	return func(t Transport) (prev Option) {
+		prev = OptDialer(t.Transport.NetDialer.(*net.Dialer))
+		t.Transport.NetDialer = d
 		return
 	}
 }
 
 // OptMux sets the muxer
-func OptMux(c yamux.Config) Option {
-	return func(t *Transport) (prev Option) {
-		prev = OptMux(t.Transport.c)
-		t.Transport.c = c
+func OptMux(c generic.MuxConfig) Option {
+	return func(t Transport) (prev Option) {
+		prev = OptMux(t.Transport.MuxConfig)
+		t.Transport.MuxConfig = c
 		return
 	}
 }
