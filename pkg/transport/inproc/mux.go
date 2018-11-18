e721007de01bcf649842444d1e3d7d7d257481c4
Louis Thibault <l.thibault@sentimens.com>
2018-11-18 03:46:15 +0000

    Performance improvement. Don't enclose type assertion in lock scope

--- pkg/transport/inproc/mux.go
+++ pkg/transport/inproc/mux.go
@@ -17,32 +17,44 @@ type radixMux struct {
 func newMux() *radixMux { return &radixMux{r: radix.New()} }
 
 func (r *radixMux) GetListener(path string) (l Listener, ok bool) {
-	r.RLock()
 	var v interface{}
-	if v, ok = r.r.Get(path); ok {
+
+	r.RLock()
+	v, ok = r.r.Get(path)
+	r.RUnlock()
+
+	if ok {
 		l = v.(Listener)
 	}
-	r.RUnlock()
+
 	return
 }
 
 func (r *radixMux) Bind(l Listener) (ln Listener, ok bool) {
-	r.Lock()
 	var v interface{}
-	if v, ok = r.r.Insert(l.Addr().String(), l); ok {
+
+	r.Lock()
+	v, ok = r.r.Insert(l.Addr().String(), l)
+	r.Unlock()
+
+	if ok {
 		ln = v.(Listener)
 	}
-	r.Unlock()
+
 	return
 }
 
 func (r *radixMux) DelListener(path string) (l Listener, ok bool) {
-	r.Lock()
 	var v interface{}
-	if v, ok = r.r.Delete(path); ok {
+
+	r.Lock()
+	v, ok = r.r.Delete(path)
+	r.Unlock()
+
+	if ok {
 		l = v.(Listener)
 	}
-	r.Unlock()
+
 	return
 }
 
