14b6a1d6f5b9d185654a559992d1c75b44ce34f6
Louis Thibault <l.thibault@sentimens.com>
2018-11-19 15:45:17 +0000

    Docstrings

--- pkg/net.go
+++ pkg/net.go
@@ -3,11 +3,18 @@ package net
 import net "github.com/lthibault/pipewerks/pkg/net"
 
 type (
-	Addr      = net.Addr
-	Error     = net.Error
+	// Addr is the standard library net.Addr
+	Addr = net.Addr
+	// Error is the standard library net.Error
+	Error = net.Error
+	// Transport is the pipewerks net.Transport
 	Transport = net.Transport
-	Listener  = net.Listener
-	Conn      = net.Conn
-	Streamer  = net.Streamer
-	Stream    = net.Stream
+	// Listener is the pipewerks net.Listener
+	Listener = net.Listener
+	// Conn is the pipewerks net.Conn
+	Conn = net.Conn
+	// Streamer is the pipewerks net.Streamer
+	Streamer = net.Streamer
+	// Stream is the pipewerks net.Stream
+	Stream = net.Stream
 )
