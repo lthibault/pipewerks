7dee04e60766d968c920a7a06e4d336ae670ee74
Louis Thibault <l.thibault@sentimens.com>
2019-05-26 17:19:17 +0200

    Revert "State handlers now take pointers to their respective Conn/Stream"
    This reverts commit 659c8c5329193bc5552f9391fd807fbd50f9dc15.

--- pkg/proto/proto.go
+++ pkg/proto/proto.go
@@ -43,24 +43,24 @@ func (s StreamState) String() string {
 
 // ConnStateHandler is called when a change to a connection state occurs
 type ConnStateHandler interface {
-	OnConnState(*pipe.Conn, ConnState)
+	OnConnState(pipe.Conn, ConnState)
 }
 
 // StreamStateHandler is called when a change to a stream state occurs
 type StreamStateHandler interface {
-	OnStreamState(*pipe.Stream, StreamState)
+	OnStreamState(pipe.Stream, StreamState)
 }
 
 // ConnStateHandlerFunc is a function that satisfies ConnStateHandler
-type ConnStateHandlerFunc func(*pipe.Conn, ConnState)
+type ConnStateHandlerFunc func(pipe.Conn, ConnState)
 
 // OnConnState calls the function that underpins ConnStateHandlerFunc
-func (h ConnStateHandlerFunc) OnConnState(c *pipe.Conn, s ConnState) { h(c, s) }
+func (h ConnStateHandlerFunc) OnConnState(c pipe.Conn, s ConnState) { h(c, s) }
 
 // StreamStateHandlerFunc is a function that satisfies ConnStateHandler
-type StreamStateHandlerFunc func(*pipe.Stream, StreamState)
+type StreamStateHandlerFunc func(pipe.Stream, StreamState)
 
 // OnStreamState calls the function that underpins StreamStateHandlerFunc
-func (h StreamStateHandlerFunc) OnStreamState(s *pipe.Stream, state StreamState) {
+func (h StreamStateHandlerFunc) OnStreamState(s pipe.Stream, state StreamState) {
 	h(s, state)
 }
